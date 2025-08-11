b523587aa95c72271eb4ffa0eb2d8c742fea7d69
vivek.walecha <vivekkumarwalecha@gmail.com>
2025-08-11 11:01:03 +0530

    ID-2 added openAI tutor created specifically for given detailed understanding of code snippets, you can give input question.

--- tutor/tutor.py
+++ tutor/tutor.py
@@ -16,6 +16,7 @@ MODEL_GPT = 'gpt-4o-mini'
 MODEL_LLAMA = 'llama3.2'
 load_dotenv()
 openai = OpenAI()
+my_question = input("Please enter your question:")
 question = """Please tell me about this code more : override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
         val view = LayoutInflater.from(viewGroup.context)
                 .inflate(R.layout.text_row_item, viewGroup, false)
@@ -24,7 +25,7 @@ question = """Please tell me about this code more : override fun onCreateViewHol
     }"""
 
 system_prompt = "You are a helpful technical tutor who answers questions about android sdk, python, software engineering, data science and LLMs"
-user_prompt = "Please give a detaaild explanation to the following question:" + question
+user_prompt = "Please give a detaaild explanation to the following question:" + my_question
 
 messages = [
     {"role": "system", "content": system_prompt},
